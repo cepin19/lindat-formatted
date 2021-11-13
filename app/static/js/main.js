62a91f20b0a75c36a63a3bc52482356d00a0eded
cepin19 <cepin19@gmail.com>
2021-11-13 15:54:58 +0100

    forgot js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -185,6 +185,88 @@ $(document).ready(function() {
 
 
   // fileupload
+
+var Upload = function (file) {
+    this.file = file;
+};
+
+Upload.prototype.getType = function() {
+    return this.file.type;
+};
+Upload.prototype.getSize = function() {
+    return this.file.size;
+};
+Upload.prototype.getName = function() {
+    return this.file.name;
+};
+Upload.prototype.doUpload = function () {
+    var that = this;
+    var formData = new FormData();
+
+    // add assoc key values, this will be posts values
+    formData.append("file", this.file, this.getName());
+    formData.append("upload_file", true);
+    formData.append("source", $("#source option:selected").val())
+    formData.append("target", $("#target option:selected").val())
+    formData.append("url", get_action_url())
+
+    $.ajax({
+        type: "POST",
+        url: "/upload",
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                myXhr.upload.addEventListener('progress', that.progressHandling, false);
+            }
+            return myXhr;
+        },
+        success: function (data) {
+                    //Convert the Byte Data to BLOB object.
+                    alert(data);
+//                    var blob = new Blob([data], { type: "application/octetstream" });
+
+                    //Check the Browser type and download the File.
+                    var isIE = false || !!document.documentMode;
+                    if (isIE) {
+                        window.navigator.msSaveBlob(blob, this.file);
+                    } else {
+                        var url = window.URL || window.webkitURL;
+                        //link = url.createObjectURL(blob);
+                        link=data;
+                        var a = $("<a />");
+                        a.attr("download", this.file);
+                        a.attr("href", link);
+                        $("body").append(a);
+                        a[0].click();
+                        $("body").remove(a);
+                    }
+                        },
+        error: function (error) {
+            // handle error
+        },
+        async: true,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        timeout: 60000
+    });
+};
+
+Upload.prototype.progressHandling = function (event) {
+    var percent = 0;
+    var position = event.loaded || event.position;
+    var total = event.total;
+    var progress_bar_id = "#progress-wrp";
+    if (event.lengthComputable) {
+        percent = Math.ceil(position / total * 100);
+    }
+    // update progressbars classes so it fits your code
+    $(progress_bar_id + " .progress-bar").css("width", +percent + "%");
+    $(progress_bar_id + " .status").text(percent + "%");
+};
+
+  // fileupload
   if (!!FileReader && 'draggable' in document.createElement('span')
 			&& !!window.FormData && "upload" in new XMLHttpRequest && !!window.Blob && !!window.FileReader) {
             var $file_field = $("input[type='file']")
@@ -197,7 +279,13 @@ $(document).ready(function() {
                             return
                         }
                         if (files[0].type !== 'text/plain'){
-                            alert(files[0].type + ' not allowed. Only text/plain.')
+                            var upload = new Upload(files[0]);
+
+                        // maby check size or type here with upload.getSize() and upload.getType()
+
+                        // execute upload
+                            upload.doUpload();
+                            //alert(files[0].type + ' not allowed. Only text/plain.')
                             event.preventDefault()
                             return
                         }
